c2234f24d5ce6f1a3c0b80ff5641e954c6407fd4
SAMBIT KUMAR MAJHI <majhisambit2@gmail.com>
2019-10-05 09:09:48 +0530

    update

--- Flutter-Projects/i_am_rich/lib/main.dart
+++ Flutter-Projects/i_am_rich/lib/main.dart
@@ -4,11 +4,17 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         appBar: AppBar(
           title: Text('I Am Rich'),
           backgroundColor: Colors.blueGrey[900],
-        );
-        body: ,
+        ),
+        body: Center(
+          child: Image(
+            image:
+                NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+          ),
+        ),
       ),
     ),
   );
