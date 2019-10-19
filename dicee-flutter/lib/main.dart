b5634dea8d58b46346a19ec5343c7ca41c268b79
SAMBIT KUMAR MAJHI <majhisambit2@gmail.com>
2019-10-19 22:31:24 +0530

    upload and update

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
@@ -15,9 +17,49 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int ldn = 1; //lEFT-DICE-NUMBER
+  int rdn = 1; //RIGHT-DICE-NUMBER
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                print('left button is pressed');
+                setState(() {
+                  ldn = Random().nextInt(6) + 1 ;
+                  rdn = Random().nextInt(6) + 1 ;
+                  print('ldn = $ldn');
+                  print('rdn = $rdn');
+                });
+              },
+              child: Image.asset('images/dice$ldn.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                print('Right button is pressed');
+                setState(() {
+                  ldn = Random().nextInt(6) + 1 ;
+                  rdn = Random().nextInt(6) + 1 ;
+                  print('rdn = $rdn');
+                  print('ldn = $ldn');
+                });
+              },
+              child: Image.asset('images/dice$rdn.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
