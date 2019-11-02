80d365d3ff81f5c8e40c3843a9d8c7e3c37ca57b
SAMBIT KUMAR MAJHI <majhisambit2@gmail.com>
2019-11-02 23:30:14 +0530

    update and upload

---
+++ bmi-calculator/lib/input_page.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'reusable_card.dart';
+import 'icon_content.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColour = Color(0xFF1D1E33);
+const inactiveCardColour = Color(0xFF111328);
+const bottomContainerColour = Color(0xFFEB1555);
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+
+  Color maleCardColour = inactiveCardColour;
+  Color femaleCardColour = inactiveCardColour;
+
+  void updateColour(int gender){
+    if(gender == 1){
+      if(maleCardColour == inactiveCardColour){
+        maleCardColour = activeCardColour;
+        femaleCardColour = inactiveCardColour;
+      } else {
+        maleCardColour = inactiveCardColour;
+      }
+    }
+    if(gender == 2){
+      if(femaleCardColour == inactiveCardColour){
+        femaleCardColour = activeCardColour;
+        maleCardColour = inactiveCardColour;
+      } else {
+        femaleCardColour = inactiveCardColour;
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+                child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: GestureDetector(
+                    onTap: (){
+                      setState(() {
+                        updateColour(1);
+                      });
+                    },
+                    child: ReuseableCard(
+                      colour: maleCardColour,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.mars,
+                        label: 'MALE',
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                    child: GestureDetector(
+                      onTap: (){
+                        setState(() {
+                          updateColour(2);
+                        });
+                      },
+                      child: ReuseableCard(
+                        colour: femaleCardColour,
+                        cardChild: IconContent(
+                          icon: FontAwesomeIcons.venus,
+                          label: 'FEMALE',
+                  ),
+                ),
+                    )),
+              ],
+            )),
+            Expanded(
+                child: ReuseableCard(
+              colour: activeCardColour,
+            )),
+            Expanded(
+                child: Row(
+              children: <Widget>[
+                Expanded(
+                    child: ReuseableCard(
+                  colour: activeCardColour,
+                )),
+                Expanded(
+                    child: ReuseableCard(
+                  colour: activeCardColour,
+                )),
+              ],
+            )),
+            Container(
+              color: bottomContainerColour,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: 80.0,
+            ),
+          ],
+        ));
+  }
+}
--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
+
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
@@ -17,6 +18,7 @@ class XylophoneApp extends StatelessWidget {
       ),
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
