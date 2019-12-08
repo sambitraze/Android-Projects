a6be07ab7c463122dde5d3ab58d43253ffc9317c
SAMBIT KUMAR MAJHI <majhisambit2@gmail.com>
2019-12-08 18:21:25 +0530

    update and upload

--- bitcoin-ticker/lib/coin_data.dart
+++ bitcoin-ticker/lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,24 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const bitcoinAverageURL =
+    'https://apiv2.bitcoinaverage.com/indices/global/ticker';
+
+class CoinData {
+  Future getCoinData(selectedCurrency) async {
+    Map<String, String> cryptoPrices = {};
+    for (String crypto in cryptoList) {
+      String requestedURL = '$bitcoinAverageURL/$crypto$selectedCurrency';
+      Response response = await get(requestedURL);
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        double lastPrice = decodedData['last'];
+        cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
+      } else {
+        print(response.statusCode);
+        throw 'Error with request code';
+      }
+    }
+    return cryptoPrices;
+  }
+}
